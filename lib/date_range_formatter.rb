5664a4d71b9f95a8894e07372a5397c292adfba5
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2024-01-12 17:56:27 +0000

    refactor: the logic is now an early return.

--- lib/date_range_formatter.rb
+++ lib/date_range_formatter.rb
@@ -18,20 +18,15 @@ class DateRangeFormatter
       return "#{full_start_date} until #{@end_time}" if @end_time
       return full_start_date if @start_time.nil? && @end_time.nil?
     else
-      if @start_time && @end_time
-        "#{full_start_date} at #{@start_time} - #{full_end_date} at #{@end_time}"
-      elsif @start_time
-        "#{full_start_date} at #{@start_time} - #{full_end_date}"
-      elsif @end_time
-        "#{full_start_date} - #{full_end_date} at #{@end_time}"
-      else
-        if same_year?
-          return @start_date.strftime("#{@start_date.day.ordinalize} - #{@end_date.day.ordinalize} %B %Y") if same_month?
-          return @start_date.strftime("#{@start_date.day.ordinalize} %B") + " - " + in_full(@end_date)
-        end
-
-        "#{full_start_date} - #{full_end_date}"
+      return "#{full_start_date} at #{@start_time} - #{full_end_date} at #{@end_time}" if @start_time && @end_time
+      return "#{full_start_date} at #{@start_time} - #{full_end_date}" if @start_time
+      return "#{full_start_date} - #{full_end_date} at #{@end_time}" if @end_time
+      if same_year?
+        return @start_date.strftime("#{@start_date.day.ordinalize} - #{@end_date.day.ordinalize} %B %Y") if same_month?
+        return @start_date.strftime("#{@start_date.day.ordinalize} %B") + " - " + in_full(@end_date)
       end
+
+      "#{full_start_date} - #{full_end_date}"
     end
   end
 
