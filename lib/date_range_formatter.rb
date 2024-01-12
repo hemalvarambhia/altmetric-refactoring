29b0ef5f105efca1b64acccbcca119bd27b8df6a
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2024-01-12 15:21:08 +0000

    refactor: copied the duplicate code to a more sensible place in preparation for removing duplication.

--- lib/date_range_formatter.rb
+++ lib/date_range_formatter.rb
@@ -19,6 +19,8 @@ class DateRangeFormatter
     elsif @start_time
       return "#{full_start_date} at #{@start_time} - #{full_end_date}" if @start_date != @end_date
       return "#{full_start_date} at #{@start_time}" if @start_date == @end_date
+    elsif @end_time
+      return "#{full_start_date} - #{full_end_date} at #{@end_time}"
     end
 
     if @start_date == @end_date
