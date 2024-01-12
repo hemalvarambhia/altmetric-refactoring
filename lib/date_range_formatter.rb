5c91960d8c36c9992f418e8420bb2fe7854db9b3
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2024-01-12 17:25:36 +0000

    refactor: added an early return so we can factor our duplication.

--- lib/date_range_formatter.rb
+++ lib/date_range_formatter.rb
@@ -33,7 +33,7 @@ class DateRangeFormatter
       "#{full_start_date} - #{full_end_date}"
       end
     elsif @start_date.year == @end_date.year
-      @start_date.strftime("#{@start_date.day.ordinalize} %B") + " - " + @end_date.strftime("#{@end_date.day.ordinalize} %B %Y")
+      return @start_date.strftime("#{@start_date.day.ordinalize} %B") + " - " + @end_date.strftime("#{@end_date.day.ordinalize} %B %Y")
     else
       "#{full_start_date} - #{full_end_date}"
     end
