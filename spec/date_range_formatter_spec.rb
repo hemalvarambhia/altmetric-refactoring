c66907da8d0996d21d1197178e30f43a29f4924d
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2024-01-12 13:58:11 +0000

    refactor: grouped similar tests together.

--- spec/date_range_formatter_spec.rb
+++ spec/date_range_formatter_spec.rb
@@ -1,19 +1,21 @@
 require "date_range_formatter"
 
 RSpec.describe(DateRangeFormatter) do
-  it "formats a date range for the same day" do
-    formatter = DateRangeFormatter.new("2009-11-1", "2009-11-1")
-    expect(formatter.to_s).to eq("1st November 2009")
-  end
+  context 'when dates are the same' do
+    it "formats a date range for the same day" do
+      formatter = DateRangeFormatter.new("2009-11-1", "2009-11-1")
+      expect(formatter.to_s).to eq("1st November 2009")
+    end
 
-  it "formats a date range for the same day with starting time" do
-    formatter = DateRangeFormatter.new("2009-11-1", "2009-11-1", "10:00")
-    expect(formatter.to_s).to eq("1st November 2009 at 10:00")
-  end
+    it "formats a date range for the same day with starting time" do
+      formatter = DateRangeFormatter.new("2009-11-1", "2009-11-1", "10:00")
+      expect(formatter.to_s).to eq("1st November 2009 at 10:00")
+    end
 
-  it "formats a date range for the same day with starting and ending times" do
-    formatter = DateRangeFormatter.new("2009-11-1", "2009-11-1", "10:00", "11:00")
-    expect(formatter.to_s).to eq("1st November 2009 at 10:00 to 11:00")
+    it "formats a date range for the same day with starting and ending times" do
+      formatter = DateRangeFormatter.new("2009-11-1", "2009-11-1", "10:00", "11:00")
+      expect(formatter.to_s).to eq("1st November 2009 at 10:00 to 11:00")
+    end
   end
 
   it "formats a date range for the same month" do
