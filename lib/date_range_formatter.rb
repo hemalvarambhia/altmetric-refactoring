190b166039ba45687b5312eb9f72a59d64e42cf9
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2024-01-14 14:51:30 +0000

    refactor: brought related states together.

--- lib/date_range_formatter.rb
+++ lib/date_range_formatter.rb
@@ -4,8 +4,9 @@ require "integer"
 class DateRangeFormatter
   def initialize(start_date, end_date, start_time = nil, end_time = nil)
     @start_date = Date.parse(start_date)
-    @end_date = Date.parse(end_date)
     @start_time = start_time
+
+    @end_date = Date.parse(end_date)
     @end_time = end_time
   end
 
