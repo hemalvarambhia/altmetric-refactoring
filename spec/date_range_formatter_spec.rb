f441995ea87c416d59bd264e589a0ce495e4c577
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2024-01-22 17:17:50 +0000

    test: clarified the scenario described in the tests.

--- spec/date_range_formatter_spec.rb
+++ spec/date_range_formatter_spec.rb
@@ -47,7 +47,7 @@ RSpec.describe(DateRangeFormatter) do
   end
 
   context 'when dates are the same' do
-    it "formats a date range for the same day" do
+    it "formats a date range for the same day and no start or end times" do
       formatter = DateRangeFormatter.new("2009-11-1", "2009-11-1")
       expect(formatter.to_s).to eq("1st November 2009")
     end
@@ -69,12 +69,12 @@ RSpec.describe(DateRangeFormatter) do
   end
 
   context 'When the year AND months are the same' do
-    it "formats a date range for the same month" do
+    it "formats a date range for the same month and no start or end times" do
       formatter = DateRangeFormatter.new("2009-11-1", "2009-11-3")
       expect(formatter.to_s).to eq("1st - 3rd November 2009")
     end
 
-    it "formats a date range for the same month with starting time" do
+    it "formats a date range for the same month with starting time and no end time" do
       formatter = DateRangeFormatter.new("2009-11-1", "2009-11-3", "10:00")
       expect(formatter.to_s).to eq("1st November 2009 at 10:00 - 3rd November 2009")
     end
@@ -85,7 +85,7 @@ RSpec.describe(DateRangeFormatter) do
     end
   end
 
-  context 'when the years are the same but the months are different' do
+  context 'when the years are the same but the months are different and times are not specified' do
     it "formats a date range for the same year" do
       formatter = DateRangeFormatter.new("2009-11-1", "2009-12-1")
       expect(formatter.to_s).to eq("1st November - 1st December 2009")
@@ -103,7 +103,7 @@ RSpec.describe(DateRangeFormatter) do
   end
 
   context 'when the years are different' do
-    it "formats a date range for different year" do
+    it "formats a date range for different year when no times are specified" do
       formatter = DateRangeFormatter.new("2009-11-1", "2010-12-1")
       expect(formatter.to_s).to eq("1st November 2009 - 1st December 2010")
     end
@@ -118,7 +118,7 @@ RSpec.describe(DateRangeFormatter) do
       expect(formatter.to_s).to eq("1st November 2009 at 10:00 - 1st December 2010 at 11:00")
     end
 
-    it 'formats a date range for the same month in different years' do
+    it 'formats a date range for the same month in different years and no times are specified' do
       formatter = DateRangeFormatter.new("2009-11-1", "2010-11-3")
       expect(formatter.to_s).to eq("1st November 2009 - 3rd November 2010")
     end
