e400080215dd6b5d5bf074bf0318b9cb1c1b7381
Jing Wu <jing.wu@simplybusiness.co.uk>
2024-01-24 11:26:12 +0000

    refactor: group tests into same category

--- spec/date_range_formatter_spec.rb
+++ spec/date_range_formatter_spec.rb
@@ -23,35 +23,37 @@ RSpec.describe(DateRangeFormatter) do
     end
   end
 
+  context 'when formatting the same month' do
+    it "formats a date range for the same month" do
+      formatter = DateRangeFormatter.new("2009-11-1", "2009-11-3")
+      expect(formatter.to_s).to eq("1st - 3rd November 2009")
+    end
+
+    it "formats a date range for the same month with starting time" do
+      formatter = DateRangeFormatter.new("2009-11-1", "2009-11-3", "10:00")
+      expect(formatter.to_s).to eq("1st November 2009 at 10:00 - 3rd November 2009")
+    end
+
+    it "formats a date range for the same month with starting and ending times" do
+      formatter = DateRangeFormatter.new("2009-11-1", "2009-11-3", "10:00", "11:00")
+      expect(formatter.to_s).to eq("1st November 2009 at 10:00 - 3rd November 2009 at 11:00")
+    end
+
+    it "formats a date range for the same month but different year " do
+      pending("Not commit failed tests since the bugs in the code before fixing it ")
+      formatter = DateRangeFormatter.new("2009-11-1", "2010-11-3")
+      expect(formatter.to_s).to eq("1st November 2009 - 3rd November 2010")
+    end
+
+    it "formats a date range for the same month but different year with time" do
+      formatter = DateRangeFormatter.new("2009-11-1", "2010-11-3", "10:00", "10:00")
+      expect(formatter.to_s).to eq("1st November 2009 at 10:00 - 3rd November 2010 at 10:00")
+    end
+  end
+
   it "formats a start day after the end day" do
     formatter = DateRangeFormatter.new("2009-12-1", "2009-11-1")
     expect(formatter.to_s).to eq("1st December - 1st November 2009")
-  end
-
-  it "formats a date range for the same month" do
-    formatter = DateRangeFormatter.new("2009-11-1", "2009-11-3")
-    expect(formatter.to_s).to eq("1st - 3rd November 2009")
-  end
-
-  it "formats a date range for the same month with starting time" do
-    formatter = DateRangeFormatter.new("2009-11-1", "2009-11-3", "10:00")
-    expect(formatter.to_s).to eq("1st November 2009 at 10:00 - 3rd November 2009")
-  end
-
-  it "formats a date range for the same month with starting and ending times" do
-    formatter = DateRangeFormatter.new("2009-11-1", "2009-11-3", "10:00", "11:00")
-    expect(formatter.to_s).to eq("1st November 2009 at 10:00 - 3rd November 2009 at 11:00")
-  end
-
-  it "formats a date range for the same month but different year " do
-    pending("Not commit failed tests since the bugs in the code before fixing it ")
-    formatter = DateRangeFormatter.new("2009-11-1", "2010-11-3")
-    expect(formatter.to_s).to eq("1st November 2009 - 3rd November 2010")
-  end
-
-  it "formats a date range for the same month but different year with time" do
-    formatter = DateRangeFormatter.new("2009-11-1", "2010-11-3", "10:00", "10:00")
-    expect(formatter.to_s).to eq("1st November 2009 at 10:00 - 3rd November 2010 at 10:00")
   end
 
   it "formats a date range for the same year" do
