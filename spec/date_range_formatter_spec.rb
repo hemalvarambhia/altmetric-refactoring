677de4d66c316e46c57cf7e64a51ace264f658c5
Jing Wu <jing.wu@simplybusiness.co.uk>
2024-01-24 11:24:54 +0000

    test: group tests together

--- spec/date_range_formatter_spec.rb
+++ spec/date_range_formatter_spec.rb
@@ -1,24 +1,26 @@
 require "date_range_formatter"
 
 RSpec.describe(DateRangeFormatter) do
-  it "formats a date range for the same day" do
-    formatter = DateRangeFormatter.new("2009-11-1", "2009-11-1")
-    expect(formatter.to_s).to eq("1st November 2009")
-  end
+  context 'when formatting the same day' do
+    it "formats a date range for the same day" do
+      formatter = DateRangeFormatter.new("2009-11-1", "2009-11-1")
+      expect(formatter.to_s).to eq("1st November 2009")
+    end
 
-  it "formats a date range for the same day with starting time" do
-    formatter = DateRangeFormatter.new("2009-11-1", "2009-11-1", "10:00")
-    expect(formatter.to_s).to eq("1st November 2009 at 10:00")
-  end
+    it "formats a date range for the same day with starting time" do
+      formatter = DateRangeFormatter.new("2009-11-1", "2009-11-1", "10:00")
+      expect(formatter.to_s).to eq("1st November 2009 at 10:00")
+    end
 
-  it "formats a date range for the same day with starting and ending times" do
-    formatter = DateRangeFormatter.new("2009-11-1", "2009-11-1", "10:00", "11:00")
-    expect(formatter.to_s).to eq("1st November 2009 at 10:00 to 11:00")
-  end
+    it "formats a date range for the same day with starting and ending times" do
+      formatter = DateRangeFormatter.new("2009-11-1", "2009-11-1", "10:00", "11:00")
+      expect(formatter.to_s).to eq("1st November 2009 at 10:00 to 11:00")
+    end
 
-  it "formats a date range for the same day but different month and different year" do
-    formatter = DateRangeFormatter.new("2009-11-1", "2010-12-1")
-    expect(formatter.to_s).to eq("1st November 2009 - 1st December 2010")
+    it "formats a date range for the same day but different month and different year" do
+      formatter = DateRangeFormatter.new("2009-11-1", "2010-12-1")
+      expect(formatter.to_s).to eq("1st November 2009 - 1st December 2010")
+    end
   end
 
   it "formats a start day after the end day" do
